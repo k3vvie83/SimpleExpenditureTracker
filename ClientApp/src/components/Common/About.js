3c7c77d4fa82112258206bde012543982ad3bf96
k3vvie83 <kevinchan83@outlook.com>
2021-06-13 15:23:57 +0800

    Edited About

--- ClientApp/src/components/Common/About.js
+++ ClientApp/src/components/Common/About.js
@@ -5,8 +5,13 @@ export function About(props) {
     return (
         <div>
             <h1> This is a simple web app for expediture tracking.  </h1>
+
             <h3> Front End is created with ReactJS  </h3>
+
             <h3> Back End is created with .NET Core 5.0  </h3>
+            <p> Additional Requirements</p>
+
+
             <h3> Database is using MySQL Community Server (Version 8.0.25)  </h3>
         </div>
     );
